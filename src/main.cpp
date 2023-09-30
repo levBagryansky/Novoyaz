2ed94b565966eb1b33239eb56d4c03630502a386
levBagryansky <28lev11@gmail.com>
2023-09-30 15:42:37 +0300

    separated rgb

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,40 @@ constexpr float SHAPE_SIZE = 8.0;
 constexpr int COUNT = 100;
 constexpr int DISPLAY_SIZE = COUNT * SHAPE_SIZE;
 
-constexpr int R_start = 20;
-constexpr int G_start = 117;
-constexpr int B_start = 104;
+constexpr int R_start = 100;
+constexpr int G_start = 100;
+constexpr int B_start = 100;
 
 constexpr int R_finish = 255;
 constexpr int G_finish = 251;
 constexpr int B_finish = 0;
-constexpr int steps = 5;
+constexpr int steps = 8;
 
-int nextR(int R) {
-    return R + (R_finish - R_start) / steps;
-}
-
-int nextG(int G) {
-    return G + (G_finish - G_start) / steps;
-}
-
-int nextB(int B) {
-    return B + (B_finish - B_start) / steps;
-}
 
 sf::RenderWindow window{ { DISPLAY_SIZE, DISPLAY_SIZE }, "CMake SFML Project" };
+
+struct RGB {
+    int R;
+    int G;
+    int B;
+    int step;
+};
 
 struct Tile {
     bool exist = false;
     int x{};
     int y{};
-    u_char R{R_start};
-    u_char G{G_start};
-    u_char B{B_start};
-    u_char alpha = 255;
+    RGB rgb;
 };
+
+RGB nextRGB(const RGB curr) {
+    return RGB {
+        .R = curr.R + (R_finish - R_start) / steps,
+        .G = curr.G + (G_finish - G_start) / steps,
+        .B = curr.B + (B_finish - B_start) / steps,
+        .step = curr.step + 1
+    };
+}
 
 void putTile(int x, int y, u_char R, u_char G, u_char B, u_char alpha = 255) {
     sf::RectangleShape rectangle{sf::Vector2f{SHAPE_SIZE, SHAPE_SIZE}};
@@ -52,10 +54,9 @@ void putTile(const Tile tile) {
         putTile(
                 tile.x,
                 tile.y,
-                tile.R,
-                tile.G,
-                tile.B,
-                tile.alpha
+                tile.rgb.R,
+                tile.rgb.G,
+                tile.rgb.B
         );
     }
 }
@@ -73,13 +74,14 @@ void loadNext(Tile *prev, Tile *next) {
                 }
                 int i = y_next * COUNT + x_next;
                 bool existed = next[i].exist;
+                RGB rgb_prev = next[i].rgb;
                 next[i] = prev[y * COUNT + x];
                 next[i].x = x_next;
                 next[i].y = y_next;
                 if (existed) {
-                    next[i].R = nextR(next[i].R);
-                    next[i].G = nextG(next[i].G);
-                    next[i].B = nextB(next[i].B);
+                    next[i].rgb = nextRGB(
+                        rgb_prev.step > next[i].rgb.step ? rgb_prev: next[i].rgb
+                    );
                 }
             }
             prev[y * COUNT + x].exist = false;
@@ -89,16 +91,23 @@ void loadNext(Tile *prev, Tile *next) {
 
 int main()
 {
-    window.setFramerateLimit(25);
+    window.setFramerateLimit(20);
     std::srand(51);
     Tile prev[COUNT * COUNT];
     Tile next[COUNT * COUNT];
+    const RGB default_rgb = RGB{
+        .R = R_start,
+        .G = G_start,
+        .B = B_start,
+        .step = 0
+    };
     for (int x = 0; x < COUNT; ++x) {
         for (int y = 0; y < COUNT; ++y) {
-            if (std::rand() % 35 == 0 && !prev[y * COUNT + x-1].exist) {
+            if (std::rand() % 20 == 0 && !prev[y * COUNT + x-1].exist) {
                 prev[y * COUNT + x].exist = true;
                 prev[y * COUNT + x].x = x;
                 prev[y * COUNT + x].y = y;
+                prev[y * COUNT + x].rgb = default_rgb;
                 ++y;
             }
         }
